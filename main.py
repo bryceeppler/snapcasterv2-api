2ee41370321296c3922b5c949b9d71b5ccddea49
Bryce Eppler <eppler97@gmail.com>
2022-10-20 18:51:00 -0700

    changing origins stucture

--- main.py
+++ main.py
@@ -28,71 +28,17 @@ origins = [
     "https://snapcaster.ca",
 ]
 
+origins = [
+    "*",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-data = [
-    {
-        "id": 1,
-        "name": 'Dockside Extortionist',
-        "set": 'Core Set 2021',
-        "price": 54.99,
-        "image":
-            'https://cards.scryfall.io/png/front/5/7/571bc9eb-8d13-4008-86b5-2e348a326d58.png?1615499802',
-            "store": '401 Games',
-            "condition": 'NM',
-            "link": 'https://store.401games.ca/pages/search-results?q=dockside+extortionist',
-    },
-    {
-        "id": 2,
-        "name": 'Dockside Extortionist',
-        "set": 'Core Set 2021',
-        "price": 54.99,
-        "image":
-            'https://cards.scryfall.io/png/front/5/7/571bc9eb-8d13-4008-86b5-2e348a326d58.png?1615499802',
-            "store": '401 Games',
-            "condition": 'NM',
-            "link": 'https://store.401games.ca/pages/search-results?q=dockside+extortionist',
-    },
-    {
-        "id": 3,
-        "name": 'Dockside Extortionist',
-        "set": 'Core Set 2021',
-        "price": 54.99,
-        "image":
-            'https://cards.scryfall.io/png/front/5/7/571bc9eb-8d13-4008-86b5-2e348a326d58.png?1615499802',
-            "store": '401 Games',
-            "condition": 'NM',
-            "link": 'https://store.401games.ca/pages/search-results?q=dockside+extortionist',
-    },
-    {
-        "id": 4,
-        "name": 'Dockside Extortionist',
-        "set": 'Core Set 2021',
-        "price": 54.99,
-        "image":
-            'https://cards.scryfall.io/png/front/5/7/571bc9eb-8d13-4008-86b5-2e348a326d58.png?1615499802',
-            "store": '401 Games',
-            "condition": 'NM',
-            "link": 'https://store.401games.ca/pages/search-results?q=dockside+extortionist',
-    },
-    {
-        "id": 5,
-        "name": 'Dockside Extortionist',
-        "set": 'Core Set 2021',
-        "price": 54.99,
-        "image":
-            'https://cards.scryfall.io/png/front/5/7/571bc9eb-8d13-4008-86b5-2e348a326d58.png?1615499802',
-            "store": '401 Games',
-            "condition": 'NM',
-            "link": 'https://store.401games.ca/pages/search-results?q=dockside+extortionist',
-    },
-]
 
 
 # Routes
