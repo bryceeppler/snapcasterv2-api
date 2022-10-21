baf7dea25a401c953e7d8cc5ae5c65b189f2c2a3
Bryce Eppler <eppler97@gmail.com>
2022-10-20 18:58:30 -0700

    adding updated origins again

--- main.py
+++ main.py
@@ -28,16 +28,13 @@ origins = [
     "https://snapcaster.ca",
 ]
 
-origins = [
-    "*",
-]
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 
