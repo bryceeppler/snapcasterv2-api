284497b23b50dda689b52c0fda32bf25da484ea1
Bryce Eppler <eppler97@gmail.com>
2022-11-09 04:54:00 -0800

    adding proper url to connection scraper

--- scrapers/sealed/ConnectionGamesSealedScraper.py
+++ scrapers/sealed/ConnectionGamesSealedScraper.py
@@ -23,11 +23,10 @@ class ConnectionGamesSealedScraper(SealedScraper):
         self.url = self.createUrl()
 
     def createUrl(self):
-        return ""
-        # urlsetName = self.setName.replace(',', '%2C').replace("'", '%27').replace(' ', '+').replace('/', '%2F')
-        # searchPrepend = '/advanced_search?utf8=%E2%9C%93&search%5Bfuzzy_search%5D=' 
-        # searchAppend = '&search%5Btags_name_eq%5D=&search%5Bsell_price_gte%5D=&search%5Bsell_price_lte%5D=&search%5Bbuy_price_gte%5D=&search%5Bbuy_price_lte%5D=&search%5Bin_stock%5D=0&search%5Bin_stock%5D=1&buylist_mode=0&search%5Bcategory_ids_with_descendants%5D%5B%5D=&search%5Bcategory_ids_with_descendants%5D%5B%5D=&search%5Bsort%5D=name&search%5Bdirection%5D=ascend&commit=Search&search%5Bcatalog_group_id_eq%5D='
-        # return self.baseUrl + searchPrepend + urlsetName + searchAppend
+        prefix = self.baseUrl + '/advanced_search?utf8=%E2%9C%93&search%5Bfuzzy_search%5D='
+        suffix = '&search%5Btags_name_eq%5D=&search%5Bsell_price_gte%5D=&search%5Bsell_price_lte%5D=&search%5Bbuy_price_gte%5D=&search%5Bbuy_price_lte%5D=&search%5Bin_stock%5D=0&search%5Bin_stock%5D=1&buylist_mode=0&search%5Bcategory_ids_with_descendants%5D%5B%5D=&search%5Bcategory_ids_with_descendants%5D%5B%5D=2796&search%5Bcategory_ids_with_descendants%5D%5B%5D=2797&search%5Bcategory_ids_with_descendants%5D%5B%5D=2799&search%5Bcategory_ids_with_descendants%5D%5B%5D=2801&search%5Bwith_descriptor_values%5D%5B2914%5D=&search%5Bwith_descriptor_values%5D%5B2915%5D=&search%5Bvariants_with_identifier%5D%5B2907%5D%5B%5D=&search%5Bsort%5D=name&search%5Bdirection%5D=ascend&commit=Search&search%5Bcatalog_group_id_eq%5D='
+        setName = self.setName.replace(',', '%2C').replace("'", '%27').replace(' ', '+').replace('/', '%2F').replace(':', '%3A')
+        return prefix + setName + suffix
 
     def scrape(self):
         page = requests.get(self.url)
@@ -42,9 +41,9 @@ class ConnectionGamesSealedScraper(SealedScraper):
             name = result.select_one('div.meta h4.name').getText()
             # get the href from the a tag with an itemprop="url" attribute
             link = self.baseUrl + result.select_one('a[itemprop="url"]')['href']
-            if 'magic_singles' not in link:
+            # if 'magic_singles' not in link:
                 # not a magic card
-                continue
+                # continue
 
             # get the set from div.meta span.category
             setName = result.select_one('div.meta span.category').getText()
@@ -56,4 +55,4 @@ class ConnectionGamesSealedScraper(SealedScraper):
                 condition = variant.select_one('span.variant-short-info').getText()
                 # price comes from the span with class = "regular price"
                 price = variant.select_one('span.regular.price').getText().replace('CAD$ ', '')
-
+                print(name, setName, condition, price, link, image)
